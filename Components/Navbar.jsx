3e047cc45da13ab93102cb897a20da3933d5c4f5
NDEGWA ABIGAIL V <102414111+AbigailVivianNdegwa@users.noreply.github.com>
2026-02-16 09:53:24 +0000

    Navbar updates -Fixed, adding Z-index

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
 
 
     return (
-        <div className="flex items-center justify-between  px-6 py-3 bg-black shadow-md text-white">
+        <div className="fixed top-0 left-0 right-0 z-50 flex items-center gap-300  px-6 py-3 bg-black shadow-md text-white">
           <div className="flex items-center"> 
             <Image 
                 src="/Images/Logo.png"
